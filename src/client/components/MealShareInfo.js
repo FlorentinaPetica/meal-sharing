cb7b98a2cce332099db6978387ed9defcec2b592
FlorentinaPetica <florentina.petica@gmail.com>
2021-12-18 18:55:16 +0100

    App.css modified

--- src/client/components/MealShareInfo.js
+++ src/client/components/MealShareInfo.js
@@ -22,12 +22,30 @@ const MealShareInfo = () => {
         ></div>
       </div>
       <div>
+        <div style={{ backgroundColor: "rgb(256, 156, 110)" }}>
+          <h2>Join the best home made meals in a cozy home!</h2>
+          <p>
+            By booking one of our dining experience you get to share a meal and
+            meet new people!
+          </p>
+        </div>
+        <div
+          style={{
+            backgroundSize: "300px",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            backgroundImage:
+              "url('https://www.eatwith.com/_next/image?url=https%3A%2F%2Fscreen-api.eatwith.com%2Ffiles%2F1432561%2F-%2Fscale_crop%2F400x600%2Fcenter%2F-%2Fprogressive%2Fyes%2F&w=1920&q=75')",
+          }}
+        ></div>
+      </div>
+      <div>
         <div style={{ backgroundColor: "rgb(146, 60, 99)" }}>
           <h2>
             Feel with the fresh taste and understand the diffrent cultures!
           </h2>
           <p>
-            Book one of our exceptional dining experiences and create unique
+            Book one of our exceptional meal and create unique
             memories that will last you a lifetime!
           </p>
         </div>
@@ -38,24 +56,6 @@ const MealShareInfo = () => {
             backgroundRepeat: "no-repeat",
             backgroundImage:
               "url('https://www.eatwith.com/_next/image?url=https%3A%2F%2Fscreen-api.eatwith.com%2Ffiles%2F1386404%2F-%2Fscale_crop%2F400x600%2Fcenter%2F-%2Fprogressive%2Fyes%2F&w=1920&q=75')",
-          }}
-        ></div>
-      </div>
-      <div>
-        <div style={{ backgroundColor: "rgb(256, 156, 110)" }}>
-          <h2>Join the best home made meals in a cozy environment!!</h2>
-          <p>
-            By booking one of our dining experience you get to share a meal and
-            meet new poeple!
-          </p>
-        </div>
-        <div
-          style={{
-            backgroundSize: "300px",
-            backgroundPosition: "center",
-            backgroundRepeat: "no-repeat",
-            backgroundImage:
-              "url('https://www.eatwith.com/_next/image?url=https%3A%2F%2Fscreen-api.eatwith.com%2Ffiles%2F1432561%2F-%2Fscale_crop%2F400x600%2Fcenter%2F-%2Fprogressive%2Fyes%2F&w=1920&q=75')",
           }}
         ></div>
       </div>
